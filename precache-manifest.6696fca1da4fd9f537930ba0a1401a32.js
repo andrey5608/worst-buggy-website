8ae779f4c75c945c42314f931f9d64b5d29b1469
andrey5608 <andrey5608@gmail.com>
2020-01-14 23:37:39 +0300

    Updates

---
+++ precache-manifest.6696fca1da4fd9f537930ba0a1401a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19699dff190b434fd87892a8df55041a",
+    "url": "/worst-buggy-website/index.html"
+  },
+  {
+    "revision": "a85919835caf313bb6af",
+    "url": "/worst-buggy-website/static/css/main.885f49bf.chunk.css"
+  },
+  {
+    "revision": "840f72931bd12c399183",
+    "url": "/worst-buggy-website/static/js/2.c5e43e66.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/worst-buggy-website/static/js/2.c5e43e66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a85919835caf313bb6af",
+    "url": "/worst-buggy-website/static/js/main.a8401ec3.chunk.js"
+  },
+  {
+    "revision": "afcf95371ef7d8402ec3",
+    "url": "/worst-buggy-website/static/js/runtime-main.25609d5f.js"
+  }
+]);
